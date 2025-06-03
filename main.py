52dbcec454ad7b03cc1ea9c206ad09bfc134cb4e
jconn5803 <jconn5803@hotmail.com>
2025-06-04 00:13:32 +0100

    Agent working

--- main.py
+++ main.py
@@ -2,10 +2,49 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
+
+class ResearchResponse(BaseModel):
+    topic:str
+    summary:str
+    sources:list[str]
+    tools_used:list[str]
+
 # Set up an LLMs
 llm = ChatOpenAI(model="gpt-4o-mini")
-response = llm.invoke("What is the meaning of life?")
-print(response)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+# Set up a prompt tempplate
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions()) # Gives the Reaerch Response to the prompt
+
+agent = create_tool_calling_agent(
+    llm = llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_repsonse = agent_executor.invoke({"query": "Who is the best golfer to ever play?"})
+print(raw_repsonse)
+
+structured_response = parser.parse(raw_repsonse.get("output")[0]["text"])
+print(structured_response)
